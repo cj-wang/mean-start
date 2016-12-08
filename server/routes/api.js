888934e701fe85a995ed35d00b9ea9b5653ecb09
Changjiang Wang <wcj3570@gmail.com>
2016-12-08 11:08:03 +1100

    Server data

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+// declare axios for making http requests
+const axios = require('axios');
+const API = 'https://jsonplaceholder.typicode.com';
+
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
+});
+
+// Get all posts
+router.get('/posts', (req, res) => {
+  // Get posts from the mock api
+  // This should ideally be replaced with a service that connects to MongoDB
+  axios.get(`${API}/posts`)
+    .then(posts => {
+      res.status(200).json(posts.data);
+    })
+    .catch(error => {
+      res.status(500).send(error)
+    });
 });
 
 module.exports = router;
