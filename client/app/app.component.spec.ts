36c89a475687a9dea5daddcfde3681922db862bd
Changjiang Wang <wcj3570@gmail.com>
2017-01-24 13:59:20 +1100

    Fix unit test

--- client/app/app.component.spec.ts
+++ client/app/app.component.spec.ts
@@ -1,16 +1,44 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from "rxjs/Rx";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
-import { RouterOutletStubComponent } from '../testing';
+import { MenuService } from './home/menu.service';
+
+class FakeMenuService {
+  getMenu() {
+    return Observable.of([
+      {
+        "title": "Menu1",
+        "sub": [
+          {
+            "title": "Menu1-Sub1",
+            "link": "menu1-1"
+          },
+          {
+            "title": "Menu1-Sub2",
+            "link": "menu1-2"
+          }
+        ]
+      }
+    ]);
+  }
+}
 
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        NgbModule.forRoot()
+      ],
       declarations: [
-        AppComponent,
-        RouterOutletStubComponent
-      ]
+        AppComponent
+      ],
+      providers: [{ provide: MenuService, useClass: FakeMenuService }]
     });
     TestBed.compileComponents();
   });
@@ -21,16 +49,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have a menu`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    fixture.detectChanges();
+    expect(app.menu).toBeTruthy();
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render menu', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('[href="/menu1-1"]').textContent).toBe('Menu1-Sub1');
+    expect(compiled.querySelector('[href="/menu1-2"]').textContent).toBe('Menu1-Sub2');
   }));
 });
